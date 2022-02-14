5a9c1e38e94ee58f72b99390b1c89ad7906bcc4b
Mark <markcfleming1990@gmail.com>
2022-02-14 17:12:58 -0600

    added server file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'hello'
+  });
+});
+
+//default response for any other request (not found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`);
+});
